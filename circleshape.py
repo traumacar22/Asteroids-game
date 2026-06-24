3e1ad74e935907308410d6b0a915878d62ed65f1
traumacar22 <patrick.ecauley@gmail.com>
2026-06-24 17:14:43 -0500

    starting ch3 l3 backup

---
+++ circleshape.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    containers: tuple[pygame.sprite.Group, ...]
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(*self.containers)
+        else:
+            super().__init__()
+
+        self.position: pygame.Vector2 = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        # must override
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,12 +11,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
+        clock = pygame.time.Clock()
+        dt =  0.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+
+        ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+        ship.draw(screen)
+        
         pygame.display.flip()
 
+        
 
 if __name__ == "__main__":
     main()
